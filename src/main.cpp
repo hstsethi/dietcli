4369e98dd55ffa22463fd66e1c1f5f895e1312e8
HstSethi <hstsethi@outlook.com>
2024-06-28 23:21:27 -0400

    Incorporate error checking into arg handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,36 @@
 #include "analysis.h"
+#include "util.h"
 #include <iostream>
 
-void printHelp(const char *executableName){
-    std::cout << executableName << " Usage : <macro|min|min> [custom consumed file path](optional)" << "\n" ;  
+void printHelp(const char *executableName) {
+  std::cout << executableName
+            << " Usage : <macro|min|min> [custom consumed file path](optional)"
+            << "\n";
 }
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-  std::cerr << "Insufficient arguments provided" << "\n";
-  printHelp(argv[0]);
-  return 1;
+    std::cerr << "Insufficient arguments" << "\n";
+    printHelp(argv[0]);
+    return 1;
   }
 
   std::string consumedFilename = "consumed.json";
 
   if (argc == 3) {
-    /* If consumedFilename is provided, use that. Otherwise fall back to`consumed.ini` */
+    /* If consumedFilename is provided, and the file is accessible, use that.
+     * Otherwise fall back to`consumed.json` */
 
-   consumedFilename = argv[2];
- }
+    if (checkFileExists(argv[2])) {
+      consumedFilename = argv[2];
+    }
+
+    else {
+      std::cerr << "No such file " << argv[2] << " falling back to consumed.json"
+                << "\n";
+      consumedFilename = "consumed.json";
+    }
+  }
 
   if (strcmp(argv[1], "macro") == 0) {
 
